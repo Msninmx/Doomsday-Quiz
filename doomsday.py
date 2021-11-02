4964148d65173e93f4d237488a695167f199f346
Miguel Nguyen <msninmx@gmail.com>
2021-11-01 19:40:11 -0600

    First commit

---
+++ doomsday.py
@@ -0,0 +1,8 @@
+from random import randint
+
+weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+year = randint(1, 2100)
+weekday = weekdays[randint(0, 6)]
+day = randint(1, 31)
+
